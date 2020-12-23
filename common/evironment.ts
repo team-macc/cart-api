f41ef2ad19467308ee0dabebdbeb2da7ede2712b
ftitonel <felipe.titonel@gmail.com>
2020-12-22 23:11:15 -0300

    Configuração serviços

--- common/evironment.ts
+++ common/evironment.ts
@@ -1,4 +1,4 @@
-// //Ambiente de DEV
+//Ambiente de DEV
 // export const environment = {
 //     server: { port: process.env.SERVER_PORT || 3001 },
 //     db: { url: process.env.DB_URL || 'mongodb://localhost/cart-api?authSource=root',
@@ -6,6 +6,16 @@
 //     security: {
 //         saltRounds: process.env.SALT_ROUNDS || 10,
 //         apiSecret: process.env.API_SECRET || 'secretKet'
+//     },
+//     services:{
+//         order:{
+//             url:'http://172.17.0.3:3002',
+//             token:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmZWxpcGUudGl0b25lbEBnbWFpbC5jb20iLCJyb2xlcyI6WyJBZG1pbiJdLCJpYXQiOjE2MDgzMTc0MTMsImV4cCI6MTY2ODMxNzQxM30.-ylgexuKIYOpI6EeGonfbG0s6swyutZGziA4ActCSGo'
+//         },
+//         stock:{
+//             url:'http://172.17.0.1:8080',
+//             token:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmZWxpcGUudGl0b25lbEBnbWFpbC5jb20iLCJyb2xlcyI6WyJBZG1pbiJdLCJpYXQiOjE2MDgzMTc0MTMsImV4cCI6MTY2ODMxNzQxM30.-ylgexuKIYOpI6EeGonfbG0s6swyutZGziA4ActCSGo'
+//         }
 //     }
 // }
 
@@ -18,5 +28,15 @@ export const environment = {
     security: {
         saltRounds: process.env.SALT_ROUNDS || 10,
         apiSecret: process.env.API_SECRET || 'secretKet'
+    },
+    services:{
+        order:{
+            url:'http://172.17.0.3:3002',
+            token:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmZWxpcGUudGl0b25lbEBnbWFpbC5jb20iLCJyb2xlcyI6WyJBZG1pbiJdLCJpYXQiOjE2MDgzMTc0MTMsImV4cCI6MTY2ODMxNzQxM30.-ylgexuKIYOpI6EeGonfbG0s6swyutZGziA4ActCSGo'
+        },
+        stock:{
+            url:'http://172.17.0.1:8080',
+            token:'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmZWxpcGUudGl0b25lbEBnbWFpbC5jb20iLCJyb2xlcyI6WyJBZG1pbiJdLCJpYXQiOjE2MDgzMTc0MTMsImV4cCI6MTY2ODMxNzQxM30.-ylgexuKIYOpI6EeGonfbG0s6swyutZGziA4ActCSGo'
+        }
     }
 }
